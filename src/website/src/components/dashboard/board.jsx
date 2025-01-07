a60eb822cf646e14143f795b10f9db128d911ad1
haeffema <haeffema@gmail.com>
2025-01-07 16:06:50 +0000

    some testing with website

---
+++ src/website/src/components/dashboard/board.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function Board() {
+    return (
+        <div>
+            <div>
+                <button disabled="true">Kategorie 1</button>
+                <button disabled="true">Kategorie 2</button>
+                <button disabled="true">Kategorie 3</button>
+                <button disabled="true">Kategorie 4</button>
+                <button disabled="true">Kategorie 5</button>
+                <button disabled="true">Kategorie 6</button>
+            </div>
+            <div>
+                <button>100</button>
+                <button>100</button>
+                <button>100</button>
+                <button>100</button>
+                <button>100</button>
+                <button>100</button>
+            </div>
+            <div>
+                <button>200</button>
+                <button>200</button>
+                <button>200</button>
+                <button>200</button>
+                <button>200</button>
+                <button>200</button>
+            </div>
+            <div>
+                <button>300</button>
+                <button>300</button>
+                <button>300</button>
+                <button>300</button>
+                <button>300</button>
+                <button>300</button>
+            </div>
+            <div>
+                <button>400</button>
+                <button>400</button>
+                <button>400</button>
+                <button>400</button>
+                <button>400</button>
+                <button>400</button>
+            </div>
+            <div>
+                <button>500</button>
+                <button>500</button>
+                <button>500</button>
+                <button>500</button>
+                <button>500</button>
+                <button>500</button>
+            </div>
+        </div>
+    )
+}
+
+export default Board;
